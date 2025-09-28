02e695e702612350d812424fed9c60025df6e879
Richard Li <79170351+RichardLi-1@users.noreply.github.com>
2025-09-28 00:49:17 -0400

    fix issues

--- src/header.tsx
+++ src/header.tsx
@@ -1,5 +1,19 @@
 export default function Header() {
     return(
-        <p> pussy</p>
+        <>
+
+            <div className="flex">
+            <div className="flex justify-between space-x-4">
+                <a href="demographics"><p>Demographics</p></a>
+                <a href="highschool"><p>High School</p></a>
+                <a href="academics"><p>Academics</p></a>
+                <a href="lifestyle"><p>Lifestyle</p></a>
+                <a href="coop"><p>Co-op</p></a>
+                <a href="photos"><p>Photos!</p></a>
+                <a href="about"><p>About</p></a>
+            </div>
+            </div>
+            
+        </>
     )
 }

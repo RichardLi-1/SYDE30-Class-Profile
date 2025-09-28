d166f8c14ecf4b0cb9d5a8455e9768111718964d
Richard Li <79170351+RichardLi-1@users.noreply.github.com>
2025-09-28 00:34:09 -0400

    PSUH

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
 import { StrictMode, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import './main.css'
-
+import Header from './header.tsx'
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
   return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold">SYDE 2030 Class Profile</h1>
-      <p className="text-gray-700 mt-2">Welcome to the SYDE 2030 Class Profile application!</p>
+    <div className="p-6 height-40vh flex">
+      <Header />
+      <h1 className="text-2xl font-bold justify-items-center">SYDE 2030 Class Profile</h1>
+      <p className="text-gray-700 mt-2 justify-items-center">Welcome to the SYDE 2030 Class Profile!</p>
     </div>
   )
 }

a911ee62f6345baca92834ec610a6284e0daa1b0
Richard Li <79170351+RichardLi-1@users.noreply.github.com>
2025-09-30 20:39:06 -0400

    change some things

--- src/header.tsx
+++ src/header.tsx
@@ -1,19 +1,30 @@
+import "./globals.css";
+
 export default function Header() {
     return(
-        <>
 
+
+        
+
+        
+        <>
+        
             <div className="flex">
-            <div className="flex justify-between space-x-4">
-                <a href="demographics"><p>Demographics</p></a>
-                <a href="highschool"><p>High School</p></a>
-                <a href="academics"><p>Academics</p></a>
-                <a href="lifestyle"><p>Lifestyle</p></a>
-                <a href="coop"><p>Co-op</p></a>
-                <a href="photos"><p>Photos!</p></a>
-                <a href="about"><p>About</p></a>
-            </div>
+                <div className="flex justify-between space-x-4">
+                    <span className="button"><a href="demographics"><p>Demographics</p></a></span>
+                    <span className="button"><a href="highschool"><p>High School</p></a></span>
+                    <span className="button"><a href="academics"><p>Academics</p></a></span>
+                    <span className="button"><a href="lifestyle"><p>Lifestyle</p></a></span>
+                    <span className="button"><a href="coop"><p>Co-op</p></a></span>
+                    <span className="button"><a href="photos"><p>Photos!</p></a></span>
+                    <span className="button"><a href="about"><p>About</p></a></span>
+                </div>
             </div>
             
+
+            <script>
+                
+            </script>
         </>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -17,6 +17,7 @@ function App() {
   )
 }
 
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
